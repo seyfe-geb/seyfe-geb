5074dd6723919e872c0b62a0363a7cfc6adb9766
seyfe-geb <syberglife@gmail.com>
2022-01-29 01:24:09 -0600

    simplecalculator changes

--- simplecalculator/simplecalculator.js
+++ simplecalculator/simplecalculator.js
@@ -1,50 +1,21 @@
 const express = require('express');
+const url = require('url');
 const app = express();
 const PORT = 3000;
+var fs = require('fs');
+const path = require('path');
 
 app.listen(PORT, (err)=>{
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
 });
 
-app.get('/calculator', (req, res, next) => {
-    res.send(`<!DOCTYPE html>
-                <html>
-                    <head>
-                        <meta charset=\"utf-8\"/>
-                        <title>Simple Calculator</title>
-                        <style>
-                            fieldset{
-                                margin:50px 500px;
-                                padding-left:100px;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <fieldset>
-                            <legend>Simple Calculator</legend>
-                            <form action="/calculate">
-                                <div>
-                                    <h2>Enter two numbers: </h2>
-                                    <input type="text" name="firstNum" placeholder="Enter first number"/><br /><br />
-                                    <input type="text" name="secondNum" placeholder="Enter second number"/><br /><br />
-                                    <label>Select operation: </label> 
-                                    <select name="operation" id="operation">
-                                        <option value="0">Add</option>
-                                        <option value="1">Subtract</option>
-                                        <option value="2">Multiply</option>
-                                        <option value="3">Divide</option>
-                                    </select>
-                                    <br><br>
-                                    <input type="submit" value="Calculate" />
-                                </div>
-                            </form>
-                        </fieldset>
-                    </body>
-                </html>`);
-});
 
-app.use('/calculate', (req, res, next) => {
+app.get('/simplecalculator', function(req, res) {
+    res.sendFile(path.join(__dirname, '/simplecalculator.html'));
+  });
+
+app.use('/calculator', (req, res, next) => {
     var q= req.query;
     var fn= parseInt(q.firstNum);
     var sn= parseInt(q.secondNum);
